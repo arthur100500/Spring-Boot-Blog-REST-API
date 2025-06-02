e7ac41fb420214147625c762f4ec8b8f680b764b
Arthur Alekseev <arthur100500@gmail.com>
2025-06-02 20:56:39 +0300

    Update deps and fixed tests
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- src/test/java/com/sopromadze/blogapi/BlogApiApplicationTests.java
+++ src/test/java/com/sopromadze/blogapi/BlogApiApplicationTests.java
@@ -1,7 +1,7 @@
 package com.sopromadze.blogapi;
 
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = BlogApiApplication.class)
 @AutoConfigureMockMvc
 public class BlogApiApplicationTests {
